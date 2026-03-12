01c9030e6583574f074c06c2202a4b7727e06c1f
Retesh07 <reteshgs2005@gmail.com>
2026-03-12 12:41:23 +0530

    Solve 0162. Find Peak Element

---
+++ 0162-find-peak-element/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 0
+        
+        if nums[0]>nums[1]:
+            return 0
+        if nums[-1]>nums[-2]:
+            return len(nums)-1
+        l=1
+        r=len(nums)-2
+
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid-1]<nums[mid]>nums[mid+1]:
+                return mid
+            elif nums[mid]<nums[mid+1]:
+                l=mid+1
+            else:
+                r=mid-1
+        return -1
+
+        
