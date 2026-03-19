26a16abd9127efc911d382fc2ec2b076ea55cc9e
Retesh07 <reteshgs2005@gmail.com>
2026-03-19 13:22:46 +0530

    Solve 1159. Smallest Subsequence of Distinct Characters

---
+++ 1159-smallest-subsequence-of-distinct-characters/solution.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        freq = Counter(s)    
+        stack = []
+        inStack = set()    
+        for ch in s:
+            freq[ch] -= 1
+            if ch in inStack:
+                continue
+
+            while stack and stack[-1] > ch and freq[stack[-1]] > 0:
+                removed = stack.pop()
+                inStack.remove(removed)
+
+          
+            stack.append(ch)
+            inStack.add(ch)
+
+        return "".join(stack)
