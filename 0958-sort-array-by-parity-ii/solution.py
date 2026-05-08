55bbc1521322f8e1d5a18b1516de59b12cb01063
Retesh07 <reteshgs2005@gmail.com>
2026-05-08 11:40:40 +0530

    Solve 0958. Sort Array By Parity II

---
+++ 0958-sort-array-by-parity-ii/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortArrayByParityII(self, nums):
+        i, j = 0, 1
+
+        while i < len(nums) and j < len(nums):
+
+            if nums[i] % 2 == 0:
+                i += 2
+
+            elif nums[j] % 2 == 1:
+                j += 2
+
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 2
+                j += 2
+
+        return nums
