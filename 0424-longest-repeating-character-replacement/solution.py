53a4a779d5da68ad7c5d368c7de8e35ed0b765bf
Retesh07 <reteshgs2005@gmail.com>
2026-06-25 12:53:59 +0530

    Solve 0424. Longest Repeating Character Replacement

---
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        m=defaultdict(int)
+        l=0
+        r=0
+        res=0
+        maxf=0
+        for r in range(len(s)):
+            m[s[r]]+=1
+            
+            maxf=max(maxf,m[s[r]])
+            while (r-l+1)-maxf>k:
+                m[s[l]]-=1
+                l+=1
+                
+            res=max(res,r-l+1)
+        return res
+
+
+        
