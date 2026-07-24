d53ff6c99e7ed0701200aee22c79b9d742f7d2e9
Retesh07 <reteshgs2005@gmail.com>
2026-07-24 14:55:25 +0530

    Solve 1878. Check if Array Is Sorted and Rotated

---
+++ 1878-check-if-array-is-sorted-and-rotated/solution.c
@@ -0,0 +1,20 @@
+bool check(int* nums, int numsSize) {
+  int count = 0;
+  int n = numsSize;
+
+    if (nums[0] < nums[n - 1]) {
+        count++;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            count++;
+        }
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+  
