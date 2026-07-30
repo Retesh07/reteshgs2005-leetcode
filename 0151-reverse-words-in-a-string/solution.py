cf29c404a9747f01933fad98d9af4e8222c4d23e
Retesh07 <reteshgs2005@gmail.com>
2026-07-30 13:10:10 +0530

    Solve 0151. Reverse Words in a String

---
+++ 0151-reverse-words-in-a-string/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseWords(self, s):
+        words = s.split()
+        res=[]
+        for i in range(len(words)-1 , -1 , -1):
+            res.append(words[i])
+            
+
+        return " ".join(res)
+        
